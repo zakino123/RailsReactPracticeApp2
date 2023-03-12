f19f57504c0ad5152ad61c50a3c25dda04021644
zakino123 <15rb076@ms.dendai.ac.jp>
2023-03-12 15:36:07 +0900

    gitignore追加

---
+++ frontend/app/src/UserList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class UserList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>UserList</h1>
+        <ul>
+          <li>田中</li>
+          <li>山田</li>
+          <li>佐藤</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default UserList;
--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import ShoppingList from './ShoppingList';
+import UserList from './UserList';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ShoppingList />
+    <UserList />
   </React.StrictMode>
 );
 
